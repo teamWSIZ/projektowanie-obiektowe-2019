f8522c12705ad80342e0975bc878a6e501be0243
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-09 09:38:18 +0100

    cd..

--- Zajecia4/src/main/java/com/threads/App.java
+++ Zajecia4/src/main/java/com/threads/App.java
@@ -13,43 +13,6 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-class runningObject extends Circle {
-    double angle;
-    double r;
-    double V_r = 10;
-    double V_a = 0;
-    double dt = 0.1;
-    double x_0 = 250;
-    double y_0 = 250;
-
-    runningObject(Group group) {
-        group.getChildren().add(this);
-
-        angle = Math.random()*360;
-
-        setRadius(5);
-        setFill(Color.BLUE);
-
-        compute();
-    }
-
-    public void compute() {
-        r += dt * V_r;
-
-        double x = x_0 + r*Math.cos(Math.toRadians(angle));
-        double y = y_0 + r*Math.sin(Math.toRadians(angle));
-
-        if (x < 0 || x > 500 || y < 0 || y > 500) {
-            r = 0;
-            x = x_0;
-            y = y_0;
-        }
-
-        setTranslateX(x);
-        setTranslateY(y);
-    }
-}
-
 public class App extends Application {
     ArrayList<runningObject> objects = new ArrayList<>();
 
