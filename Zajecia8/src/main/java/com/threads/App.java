809edcac97e90ffe3de0e359e87f19c3a4e38fc1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-30 08:51:25 +0100

    cd..

--- Zajecia8/src/main/java/com/threads/App.java
+++ Zajecia8/src/main/java/com/threads/App.java
@@ -6,7 +6,40 @@ package com.threads;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+
+class Ship{
+    Rectangle rect = new Rectangle();
+    Scene scene;
+    double width;
+    double height;
+
+    double bottomMargin = 5;
+
+    Ship(Group group, Scene scene, double width, double height){
+
+        this.width = width;
+        this.height = height;
+
+        this.scene = scene;
+
+        rect.setWidth(this.width);
+        rect.setHeight(this.height);
+
+        rect.setFill(Color.BLUE);
+
+        group.getChildren().add(rect);
+
+        redraw();
+    }
+    public void redraw(){
+        rect.setTranslateX(scene.getWidth()/2-width/2);
+        rect.setTranslateY(scene.getHeight()-height-bottomMargin);
+    }
+}
 
 public class App extends Application {
 
@@ -17,5 +50,7 @@ public class App extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Ship ship = new Ship(group,scene,50,20);
     }
 }
