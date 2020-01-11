17d965fbe08c12ddb4f406970c988115cbcc8a80
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-01-11 09:40:39 +0100

    cd..

--- Zajecia11/src/main/java/com/example/factory/App.java
+++ Zajecia11/src/main/java/com/example/factory/App.java
@@ -3,13 +3,29 @@
  */
 package com.example.factory;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        Car bmw = new Bmw("2.5","19","450hp","Coupe");
-        Car opel = new OpelInsignia("2.0","19","250hp","Coupe");
+        ArrayList<Car> cars = new ArrayList<>();
 
-        bmw.print();
-        opel.print();
+        for(int i=0;i<100;i++){
+            Car car;
+            switch((int)(Math.random()*2+1)){
+                case 1:
+                    car = CarFactory.createCar("BMW","2.5","19","450hp","Coupe");
+                    cars.add(car);
+                    break;
+                case 2:
+                    car = CarFactory.createCar("OpelInsignia","2.0","19","250hp","Coupe");
+                    cars.add(car);
+                    break;
+            }
+        }
+
+
+        for(Car car : cars)
+            car.print();
     }
 }
 
---
+++ Zajecia11/src/main/java/com/example/factory/CarFactory.java
@@ -0,0 +1,13 @@
+package com.example.factory;
+
+class CarFactory{
+    static Car createCar(String brand,String engine, String wheels, String power, String body){
+        if(brand.equalsIgnoreCase("bmw"))
+            return new Bmw(engine,wheels,power,body);
+
+        if(brand.equalsIgnoreCase("OpelInsignia"))
+            return new OpelInsignia(engine,wheels,power,body);
+
+        return null;
+    }
+}
