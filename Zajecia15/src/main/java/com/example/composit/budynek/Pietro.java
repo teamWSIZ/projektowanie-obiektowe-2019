0c3ee90e1c9a1c776f54f6ae8597572ccbf413c8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-01 09:46:19 +0100

    cd..

--- Zajecia15/src/main/java/com/example/composit/budynek/Pietro.java
+++ Zajecia15/src/main/java/com/example/composit/budynek/Pietro.java
@@ -1,4 +1,28 @@
 package com.example.composit.budynek;
 
-public class Pietro {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class Pietro implements Pomieszczenie {
+    ArrayList<Pomieszczenie> pomieszczenia = new ArrayList<>();
+
+    @Override
+    public @NotNull String getName() {
+        return "Piętro";
+    }
+
+    @Override
+    public @NotNull Double powierzchnia() {
+        Double powierzchnia = 0.0;
+
+        for (Pomieszczenie pomieszczenie : pomieszczenia)
+            powierzchnia += pomieszczenie.powierzchnia();
+
+        return powierzchnia;
+    }
+
+    public void dodajPomieszczenie(Pomieszczenie pomieszczenie) {
+        pomieszczenia.add(pomieszczenie);
+    }
 }
