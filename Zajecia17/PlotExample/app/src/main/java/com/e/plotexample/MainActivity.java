280167beb1f1b6801e2b34f47c8c1b214a94f6e8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-21 14:21:10 +0100

    plot added..

--- Zajecia17/PlotExample/app/src/main/java/com/e/plotexample/MainActivity.java
+++ Zajecia17/PlotExample/app/src/main/java/com/e/plotexample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.e.plotexample;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        series.appendData(new DataPoint(10,10),false,100);
+        graph.addSeries(series);
     }
 }
